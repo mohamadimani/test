d94d9aafa79ee88e182df69442569c5c6f540eeb
mohamadimani <mohamadimahdi@yahoo.com>
2021-10-01 13:21:07 +0330

    first commit

---
+++ benz.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once 'cars.php';
+
+class benz extends cars
+{
+    private static $color;
+
+    public  function Color($color = null)
+    {
+        benz::$color = !empty($color) ? $color : cars::getColor();
+    }
+
+    public static function gColor()
+    {
+        benz::Color();
+        return benz::$color;
+    }
+
+}
+
+//$thiss = new benz();
+print_r("color = ");
+print_r(benz::gColor());
