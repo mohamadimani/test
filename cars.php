89cc3768266baccc248311f7b90341ea932515c9
mohamadimani <mohamadimahdi@yahoo.com>
2021-10-01 13:21:27 +0330

    first commit

---
+++ cars.php
@@ -0,0 +1,44 @@
+<?php
+
+class cars
+{
+
+    private static $name = 'benz';
+    private static $color = 'red';
+    private static $price = '1200000';
+
+    private static function setName($name)
+    {
+        self::$name = $name;
+    }
+
+    private static function getName()
+    {
+        
+        return cars::$name;
+    }
+
+    protected static function setColor($color)
+    {
+        self::$color = $color;
+    }
+
+    protected static function getColor()
+    {
+        return cars::$color;
+    }
+
+    protected static function setPrice($price)
+    {
+        cars::$price = $price;
+    }
+
+    protected static function getPrice()
+    {
+        return self::$price;
+    }
+
+
+}
+
+
